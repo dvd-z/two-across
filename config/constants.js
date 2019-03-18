3601212f5f16c400703fffe02feeefe0b32e7b4e
David Zhou <david.dz.zhou@gmail.com>
2019-03-17 20:24:02 -0400

    Implement puppeteer

---
+++ config/constants.js
@@ -0,0 +1,7 @@
+module.exports = {
+    SOURCES: new Map([
+        ['nyt', 'https://nytimes.com/crosswords'],
+        ['wapo', 'https://washingtonpost.com/crossword-puzzles/daily/'],
+        ['wsj', 'https://blogs.wsj.com/puzzle/category/crossword/']
+    ])
+};
--- server.js
+++ server.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const puppeteer = require('puppeteer');
+const Constants = require('./config/constants');
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 app.get('/', (req, res) => {
     res.send('Hello world!');
+});
+
+app.get('/parse/:src', async (req, res) => {
+    if (!Constants.SOURCES.has(req.params.src)) {
+        res.sendStatus(400);
+    }
+    const url = Constants.SOURCES.get(req.params.src);
+
+    puppeteer
+        .launch()
+        .then(browser => browser.newPage())
+        .then(async page => {
+            await page.goto(url);
+            return await page.content();
+        })
+        .then()
+        .catch(err => console.error(err));
+
+    res.sendStatus(200);
 });
