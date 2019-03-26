72db37fa5cef7b9f07fc900d5e9be52f1eb75043
David Zhou <david.dz.zhou@gmail.com>
2019-03-25 21:10:32 -0400

    Add click instructions to reveal grid for NYT

--- config/parseMaps.js
+++ config/parseMaps.js
@@ -2,6 +2,14 @@ const Constants = require('./constants');
 const nyt = require('../parse/nyt');
 
 module.exports = {
+    SOURCE_CLICK_REVEAL_MAP: new Map([
+        [Constants.NYT, [
+            "button.buttons-modalButton--1REsR > div",
+            "div.Toolbar-expandedMenu--2s4M4 > li:nth-child(2) > button",
+            "div.Toolbar-expandedMenu--2s4M4 > li:nth-child(2) > ul > li:nth-child(3)",
+            "div.buttons-modalButtonContainer--35RTh > button:nth-child(2)"
+        ]]
+    ]),
     SOURCE_PARSE_BUTTON_MAP: new Map([
         [Constants.NYT, null]
     ]),
