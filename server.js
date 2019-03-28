d53d2e279ff0730feab1fe14613463b13f48042f
David Zhou <david.dz.zhou@gmail.com>
2019-03-27 22:30:23 -0400

    Set up Firebase

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 5000;
+const firebase = require('firebase');
+firebase.initializeApp({
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+});
 const puppeteer = require('puppeteer');
 const chunk = require('lodash/chunk');
 const Crossword = require('./models/Crossword');
