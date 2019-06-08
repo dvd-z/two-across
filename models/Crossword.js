bbb5a36b84e393d7da3968af1fae242fa20f3c4e
David Zhou <david.dz.zhou@gmail.com>
2019-06-08 18:22:09 -0400

    Update Crossword class

--- models/Crossword.js
+++ models/Crossword.js
@@ -5,12 +5,12 @@ const Constants = require('../config/constants');
  * Class representing a Crossword.
  */
 module.exports = class Crossword {
-    // TODO: add date, author, editor, name properties
+    // TODO: add author, editor, name properties
     /**
      * Create a crossword.
      * @param {string} source - The source of the crossword (e.g. NYT).
      * @param {Date} date - The date of the crossword in an interpretable format
-     * @param {Object.<string, [][]>} clues
+     * @param {Object.<string, string[][]>} clues
      * @param {Cell[][]} grid - The representation of the grid. 
      */
     constructor(source, date, clues, grid) {
